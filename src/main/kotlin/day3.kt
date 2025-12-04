2700472c321b3366b330077d9dd8663b70566adb
Jesper Evertsson <jesper@piktiv.se>
2025-12-04 15:42:54 +0100

    Add solution for day 3 part 1

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -1,5 +1,27 @@
 package main.kotlin
 
 fun day3 (lines: List<String>) {
+    var totalJoltage = 0L
 
+    lines.forEach { line ->
+        var largestNumber = -1
+        var secondLargestNumber = -1
+        var largestNumberPos = -1
+        for (i in 0 until line.length - 1) {
+            if (Integer.parseInt(line[i].toString(), 10) > largestNumber) {
+                largestNumber = Integer.parseInt(line[i].toString(), 10)
+                largestNumberPos = i
+            }
+        }
+
+        for (i in largestNumberPos + 1 until line.length) {
+            if (Integer.parseInt(line[i].toString(), 10) > secondLargestNumber) {
+                secondLargestNumber = Integer.parseInt(line[i].toString(), 10)
+            }
+        }
+
+        totalJoltage += (largestNumber.toString() + secondLargestNumber.toString()).toLong(10)
+    }
+
+    println("Day 3 part 1: $totalJoltage")
 }

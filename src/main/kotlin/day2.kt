13f7ba333abf310b26c828d878ce9c32d5511901
Jesper Evertsson <jesper@piktiv.se>
2025-12-04 12:06:41 +0100

    Add solution for day 2 part 2 (yes, it's the worst code ever written)

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -2,6 +2,7 @@ package main.kotlin
 
 fun day2 (lines: List<String>) {
     day2part1(lines[0])
+    day2part2(lines[0])
 }
 
 fun day2part1(input: String) {
@@ -39,4 +40,83 @@ fun day2part1(input: String) {
 
 fun day2part2(input: String) {
     val ranges = input.split(',')
+
+    var part2Answer = 0L
+
+    ranges.forEach { range ->
+        val start = (range.split('-')[0]).toLong(10)
+        val end = (range.split('-')[1]).toLong(10)
+
+        for (i in start..end) {
+            val id = i.toString(10)
+
+            if (id.length == 2 && id[0] == id[1]) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 3 && id[0] == id[1] && id[1] == id[2]) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 4 && (
+                (id[0] == id[1] && id[1] == id[2] && id[2] == id[3])
+                || (id[0] == id[2] && id[1] == id[3])
+            )) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 5 && id[0] == id[1] && id[1] == id[2] && id[2] == id[3] && id[3] == id[4]) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 6 && (
+                (id[0] == id[1] && id[1] == id[2] && id[2] == id[3] && id[3] == id[4] && id[4] == id[5])
+                || (id[0] == id[3] && id[1] == id[4] && id[2] == id[5])
+                || (id[0] == id[2] && id[2] == id[4] && id[1] == id[3] && id[3] == id[5])
+            )) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 7 && id[0] == id[1] && id[1] == id[2] && id[2] == id[3] && id[3] == id[4] && id[4] == id[5]
+                && id[5] == id[6]) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 8 && (
+                (id[0] == id[1] && id[1] == id[2] && id[2] == id[3] && id[3] == id[4] && id[4] == id[5] && id[5] == id[6] && id[6] == id[7])
+                    || (id[0] == id[4] && id[1] == id[5] && id[2] == id[6] && id[3] == id[7])
+                    || (id[0] == id[2] && id[2] == id[4] && id[4] == id[6] && id[1] == id[3] && id[3] == id[5] && id[5] == id[7])
+            )) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 9 && ((id[0] == id[1] && id[1] == id[2] && id[2] == id[3] && id[3] == id[4] && id[4] == id[5]
+                && id[5] == id[6] && id[6] == id[7] && id[7] == id[8]) || (
+                    id[0] == id[3] && id[3] == id[6]
+                    && id[1] == id[4] && id[4] == id[7]
+                    && id[2] == id[5] && id[5] == id[8]
+                ))) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+
+            if (id.length == 10 && (
+                (id[0] == id[1] && id[1] == id[2] && id[2] == id[3] && id[3] == id[4] && id[4] == id[5] && id[5] == id[6] && id[6] == id[7] && id[7] == id[8] && id[8] == id[9])
+                    || (id[0] == id[5] && id[1] == id[6] && id[2] == id[7] && id[3] == id[8] && id[4] == id[9])
+                    || (id[0] == id[2] && id[2] == id[4] && id[4] == id[6] && id[6] == id[8] && id[1] == id[3] && id[3] == id[5] && id[5] == id[7] && id[7] == id[9])
+            )) {
+                part2Answer += (id).toLong(10)
+                continue
+            }
+        }
+    }
+
+    println("Day 2 part 2: $part2Answer")
 }

d2a53495981584571f41c52e8822ced9dfdd2b94
Jesper Evertsson <jesper@piktiv.se>
2025-12-04 15:56:44 +0100

    Add solution for day 3 part 2

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -1,6 +1,11 @@
 package main.kotlin
 
 fun day3 (lines: List<String>) {
+    day3part1(lines)
+    day3part2(lines)
+}
+
+fun day3part1(lines: List<String>) {
     var totalJoltage = 0L
 
     lines.forEach { line ->
@@ -25,3 +30,40 @@ fun day3 (lines: List<String>) {
 
     println("Day 3 part 1: $totalJoltage")
 }
+
+fun day3part2(lines: List<String>) {
+    var totalJoltage = 0L
+
+    lines.forEach { line ->
+        var startIndex = 0
+        var endIndex = line.length - 11
+        var largestJoltage = ""
+
+        repeat(12) {
+            val result = findLargestNumber(line, startIndex, endIndex)
+
+            largestJoltage += result.number.toString()
+            startIndex = result.position + 1
+            endIndex++
+        }
+
+        totalJoltage += largestJoltage.toLong()
+    }
+
+    println("Day 3 part 2: $totalJoltage")
+}
+
+fun findLargestNumber(line: String, startIndex: Int, endIndex: Int): JoltageHelper {
+    var largestNumber = -1
+    var largestNumberPos = -1
+    for (i in startIndex until endIndex) {
+        if (Integer.parseInt(line[i].toString(), 10) > largestNumber) {
+            largestNumber = Integer.parseInt(line[i].toString(), 10)
+            largestNumberPos = i
+        }
+    }
+
+    return JoltageHelper(largestNumber, largestNumberPos)
+}
+
+data class JoltageHelper(val number: Int, val position: Int)

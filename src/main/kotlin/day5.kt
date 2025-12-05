d8f08f27041cac7d9c0ca0a572d3a2db46e8b1ba
Jesper Evertsson <jesper@piktiv.se>
2025-12-05 11:06:45 +0100

    Add solution for day 5 part 1

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -1,5 +1,13 @@
 package main.kotlin
 
 fun day5 (lines: List<String>) {
+    var freshIds = 0
 
+    day5Ids.forEach { id ->
+        if (day5Ranges.any { id >= it.first && id <= it.second }) {
+            freshIds++
+        }
+    }
+
+    println("Day 5 part 1: $freshIds")
 }

e6e2c47ff2d7605319e780b3a8b2483e08bafa95
Jesper Evertsson <jesper@piktiv.se>
2025-12-06 11:08:21 +0100

    Add solution for day 6 part 1

--- src/main/kotlin/day6.kt
+++ src/main/kotlin/day6.kt
@@ -1,5 +1,25 @@
 package main.kotlin
 
-fun day6 (lines: List<String>) {
+import java.util.regex.Pattern
 
+fun day6 (lines: List<String>) {
+    val pattern: Pattern = Pattern.compile("\\s")
+    val line1 = lines[0].split(pattern).filter { it.isNotEmpty() }
+    val line2 = lines[1].split(pattern).filter { it.isNotEmpty() }
+    val line3 = lines[2].split(pattern).filter { it.isNotEmpty() }
+    val line4 = lines[3].split(pattern).filter { it.isNotEmpty() }
+
+    val operators = lines[4].split(pattern).filter { it.isNotEmpty() }
+
+    var sum = 0L
+
+    for (i in 0 until line1.size) {
+        if (operators[i] == "+") {
+            sum += (line1[i].toLong() + line2[i].toLong() + line3[i].toLong() + line4[i].toLong())
+        } else {
+            sum += (line1[i].toLong() * line2[i].toLong() * line3[i].toLong() * line4[i].toLong())
+        }
+    }
+
+    println("Day 6 part 1: $sum")
 }

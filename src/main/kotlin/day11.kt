a0397797afe019149c97ca25828fe0bfaf01bfe5
Jesper Evertsson <jesper@piktiv.se>
2025-12-15 19:04:55 +0100

    Add solution for day 11 part 1

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,5 +1,39 @@
 package main.kotlin
 
 fun day11 (lines: List<String>) {
-    
+    val devices: Map<String, MutableSet<String>> = lines.associate { line ->
+        val key = line.split(": ")[0]
+        val values = line.split(": ")[1].split(" ").toMutableSet()
+
+        Pair(key, values)
+    }
+
+    val start = devices["you"]!!
+    var paths = mutableListOf<MutableSet<String>>()
+    var routeCount = 0
+
+    start.forEach {
+        paths.add(mutableSetOf(it))
+    }
+
+    while(paths.isNotEmpty()) {
+        val newPaths = mutableListOf<MutableSet<String>>()
+
+        paths.forEach { path ->
+            devices[path.last()]!!.forEach { neighbor ->
+                if (neighbor == "out") {
+                    routeCount++
+                }
+                else if (!path.contains(neighbor) && neighbor != "you") {
+                    val extendedPath = path.toMutableSet()
+                    extendedPath.add(neighbor)
+                    newPaths.add(extendedPath)
+                }
+            }
+        }
+
+        paths = newPaths
+    }
+
+    println("Day 11 part 1: $routeCount")
 }

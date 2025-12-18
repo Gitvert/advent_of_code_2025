38d8358f7994d2a5332846c2eda243535adfb36b
Jesper Evertsson <jesper@piktiv.se>
2025-12-18 12:48:53 +0100

    Add solution for day 11 part 2

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,6 +1,13 @@
 package main.kotlin
 
+val deviceCache = hashMapOf<DeviceCacheKey, Long>()
+
 fun day11 (lines: List<String>) {
+    day11Part1(lines)
+    day11Part2(lines)
+}
+
+fun day11Part1(lines: List<String>) {
     val devices: Map<String, MutableSet<String>> = lines.associate { line ->
         val key = line.split(": ")[0]
         val values = line.split(": ")[1].split(" ").toMutableSet()
@@ -24,7 +31,7 @@ fun day11 (lines: List<String>) {
                 if (neighbor == "out") {
                     routeCount++
                 }
-                else if (!path.contains(neighbor) && neighbor != "you") {
+                else if (!path.contains(neighbor)) {
                     val extendedPath = path.toMutableSet()
                     extendedPath.add(neighbor)
                     newPaths.add(extendedPath)
@@ -37,3 +44,49 @@ fun day11 (lines: List<String>) {
 
     println("Day 11 part 1: $routeCount")
 }
+
+fun day11Part2(lines: List<String>) {
+    val devices: Map<String, MutableSet<String>> = lines.associate { line ->
+        val key = line.split(": ")[0]
+        val values = line.split(": ")[1].split(" ").toMutableSet()
+
+        Pair(key, values)
+    }
+
+    val routeCount = findPathsToOut("svr", false, false, devices)
+
+    println("Day 11 part 2: $routeCount")
+}
+
+fun findPathsToOut(device: String, visitsDac: Boolean, visitsFft: Boolean, devices: Map<String, MutableSet<String>>): Long {
+    if (device == "out") {
+        if (visitsFft && visitsDac) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+
+    val cacheKey = DeviceCacheKey(device, visitsDac, visitsFft)
+
+    if (deviceCache.contains(cacheKey)) {
+        return deviceCache[cacheKey]!!
+    }
+
+    var pathsOut = 0L
+
+    devices[device]!!.forEach {
+        pathsOut += findPathsToOut(
+            it,
+            visitsDac || device == "dac",
+            visitsFft || device == "fft",
+            devices
+        )
+    }
+
+    deviceCache[cacheKey] = pathsOut
+
+    return pathsOut
+}
+
+data class DeviceCacheKey(val device: String, val visitsDac: Boolean, val visitsFft: Boolean)

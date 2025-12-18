04a933500fd99afbc305ed3f4ad5db3e216d978f
Jesper Evertsson <jesper@piktiv.se>
2025-12-18 18:58:17 +0100

    Add solution for day 12 part 1

--- src/main/kotlin/day12.kt
+++ src/main/kotlin/day12.kt
@@ -1,5 +1,29 @@
 package main.kotlin
 
+val figSizes = listOf(7, 7, 6, 7, 5, 7)
+
 fun day12 (lines: List<String>) {
-    
+    val answer = lines.sumOf { line ->
+        val sizeString = line.split(":").first()
+        val height = sizeString.split("x").first().toLong()
+        val width = sizeString.split("x")[1].toLong()
+
+        val spaceAvailable = height * width
+
+        val counts = line.split(": ")[1].split(" ").map { it.toLong() }
+
+        var totalSize = 0L
+
+        counts.forEachIndexed { i, count ->
+            totalSize += count * figSizes[i]
+        }
+
+        if (totalSize < spaceAvailable) {
+            1
+        } else {
+            0
+        }
+    }
+
+    println("Day 12 part 1: $answer")
 }
